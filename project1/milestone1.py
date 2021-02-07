e4f84ee8a0c10feb9bc55ca01fa7b7816d919e7b
tejusunkara <jnanatejaswini@gmail.com>
2021-02-07 02:05:19 +0000

    Imported Flask, was able to get top tracks for chosen artist along with album images

--- project1/milestone1.py
+++ project1/milestone1.py
@@ -1,34 +1,52 @@
+from flask import Flask, render_template
 from dotenv import load_dotenv, find_dotenv
 import requests
 import os
 import json
 
 load_dotenv(find_dotenv())
+
+#connecting to API
 auth_url='https://accounts.spotify.com/api/token'
-# data = {
-#     'grant_type': 'client_credentials',
-#     'client_id': os.getenv('CLIENT_ID'),
-#     'client_secret': os.getenv('CLIENT_SECRET')
-# }
-    
 response = requests.post(auth_url, {
     'grant_type': 'client_credentials',
     'client_id': os.getenv('CLIENT_ID'),
-    'client_secret': os.getenv('CLIENT_SECRET'),
+    'client_secret': os.getenv('CLIENT_SECRET')
 })
 json_response = response.json()
 access_token = json_response['access_token']
 
-url='https://api.spotify.com/v1/artists/0hCNtLu0JehylgoiP8L4Gh/top-tracks?market=US'    #artist=nicki minaj
-#artist_id='0hCNtLu0JehylgoiP8L4Gh?si=5NaHOy9LQhi7JY-kCSgAvw'
+app = Flask(__name__)
+
+artist_id='0hCNtLu0JehylgoiP8L4Gh'
+spot_url='https://api.spotify.com/v1/artists/'+artist_id+'/top-tracks?market=US'    #artist=nicki minaj
+market='market=US'
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
-music=requests.get(url, headers=headers)
-#print(music.content)
+music=requests.get(spot_url, headers=headers)
 top_tracks = music.json()
+tracks=[]
+track_images={}
 
-for track in range(0,10):
-    print(top_tracks['tracks'][track]['name'])
+@app.route('/')
+def topten():
+    for track in range(0,10):
+        tracks.append(top_tracks['tracks'][track]['name'])
+        track_images[track] = top_tracks['tracks'][track]['album']['images'][0]['url']
+    print("track images values: \n" +str(list(track_images)))
     
+    return render_template(
+        'index.html', 
+        tracks=tracks,
+        track_images=track_images,
+        artist = 'Nicki Minaj',
+        tracklen = len(tracks),
+    )
+    
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
